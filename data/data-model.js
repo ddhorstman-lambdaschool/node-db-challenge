2ac3d577df274a1d04d70eec66d040bf2c1f526c
David Horstman <davidzilla27@gmail.com>
2020-05-31 23:01:14 -0400

    GET requests

--- data/data-model.js
+++ data/data-model.js
@@ -1,16 +1,35 @@
-const db = require("./dbConfig");
+const knex = require("./dbConfig");
 
 function addProject() {}
 function addTask(project_id) {}
-function getProject(id) {}
-function getTasks(project_id) {}
+function getProjects() {
+  return knex("projects");
+}
+function getProject(id) {
+  return knex("projects").where({ id });
+}
+function getTasks(project_id) {
+  return knex("tasks")
+    .where(project_id ? { project_id } : {})
+    .join("projects", "projects.id", "tasks.id")
+    .select(
+      "tasks.description",
+      "tasks.notes",
+      "tasks.completed",
+      "projects.name as project_name",
+      "projects.description as project_description"
+    );
+}
 function addResource() {}
-function getResource(id) {}
+function getResources() {
+  return knex("resources");
+}
 module.exports = {
   addProject,
   addTask,
+  getProjects,
   getProject,
   getTasks,
   addResource,
-  getResource,
+  getResources,
 };
--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 const express = require("express");
 const server = express();
 const PORT = process.env.PORT || 5000;
+const { custom404, errorHandling, catchAsync, AppError } = require("./errors");
 
 const db = require("./data/data-model");
+const validateProjID = catchAsync(validateProjectID);
+/*----------------------------------------------------------------------------*/
+/* GET
+/*----------------------------------------------------------------------------*/
+server.get(
+  "/api/projects",
+  catchAsync(async (req, res) => {
+    const projects = await db.getProjects();
+    res.status(200).json(projects);
+  })
+);
+server.get(
+  "/api/resources",
+  catchAsync(async (req, res) => {
+    const resources = await db.getResources();
+    res.status(200).json(resources);
+  })
+);
+server.get(
+  "/api/tasks",
+  catchAsync(async (req, res) => {
+    const tasks = await db.getTasks();
+    res.status(200).json(tasks);
+  })
+);
+server.get(
+  "/api/:id/tasks",
+  validateProjID,
+  catchAsync(async (req, res) => {
+    const { id } = req.project;
+    const tasks = await db.getTasks(id);
+    res.status(200).json(tasks);
+  })
+);
+
+/*----------------------------------------------------------------------------*/
+/* Errors and Listen
+/*----------------------------------------------------------------------------*/
+
+server.all("*", custom404);
+server.use(errorHandling);
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+/*----------------------------------------------------------------------------*/
+/* Middleware
+/*----------------------------------------------------------------------------*/
+async function validateProjectID(req, res, next) {
+  const { id } = req.params;
+  //projects are returned as an array - we just want the first entry
+  const [project] = await db.getProject(id);
+  req.project = project;
+  project ? next() : next(new AppError(`${id} is not a valid project ID`, 404));
+}
